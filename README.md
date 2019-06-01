f24699e306c7bcfcd1447d270f83fe78ab17ab68
Cherish7777 <13133919932@163.com>
2019-06-01 13:34:44 +0800

    day1

--- README.md
+++ README.md
@@ -6,3 +6,15 @@
 1. 完成Header区域，使用的是Mint-UI中的Header组件
 2. 制作地步的Tabbar区域，使用的是MUI的Tabbar.html
 3. 要在中间区域放置一个router-view来展示路由匹配到的组件
+
+## 改造 tabbar为router-link
+
+## 设置路由高亮
+
+## 点击 tabbar中的路由连接，展示对应的路由组件
+
+## 制作首页轮播图布局
+## 加载首页轮播图数据
+1. 使用axios获取数据 this.$axios.get
+2. 获取到的数据，保存到data上，
+3. 使用v-for循环渲染每个item项
--- src/main.js
+++ src/main.js
@@ -2,13 +2,31 @@
 
 import Vue from 'vue'
 
+// 导入路由
+import VueRouter from 'vue-router'
+// 安装路由
+Vue.use(VueRouter)
+
+// 导入 axios
+import axios from 'axios'
+// import VueAxios from 'vue-axios'
+// 安装 axios
+// Vue.use(axios)
+Vue.prototype.$axios = axios
+
 // 导入MUI的样式
 import './lib/mui/css/mui.min.css'
 import './lib/mui/css/icons-extra.css'
 
 // 按需导入 Mint-UI 中的组件
-import { Header } from 'mint-ui'
+import { Header, Swipe, SwipeItem } from 'mint-ui'
 Vue.component(Header.name,Header)
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
+
+// 导入路由模块，router.js
+import router from './router'
+
 
 // 导入App根组件
 import app from './App.vue' 
@@ -17,10 +35,7 @@ var vm = new Vue({
   el: '#app',
   data: {},
   methods: {},
-  created() {
-
-  },
-  render(h) {
-    return h(app)
-  },
+  router,
+  render: h => h(app),
+  
 })
